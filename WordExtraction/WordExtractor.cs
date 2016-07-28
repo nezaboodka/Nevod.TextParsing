f11b04c7bce8711fe5e43bdfd6a45ed7481fe59d
Ivan <vana.shimko@gmail.com>
2016-07-28 20:39:29 +0300

    Add ScanWindow for look(behind/ahead).

--- WordExtraction/WordExtractor.cs
+++ WordExtraction/WordExtractor.cs
@@ -1,15 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace WordExtraction
 {
     class WordExtractor
     {
+        class ScanWindow
+        {
+            public WordBreakPropertyType? Ahead { get; private set; }
+
+            public WordBreakPropertyType? Current { get; private set; }
+
+            public WordBreakPropertyType? Behind { get; private set; }
+
+            public WordBreakPropertyType? BehindOfBehind { get; private set; }
+
+            public void Add(WordBreakPropertyType? nextSymbolType)
+            {
+                BehindOfBehind = Behind;
+                Behind = Current;
+                Current = Ahead;
+                Ahead = nextSymbolType;
+            }
+
+            public ScanWindow(WordBreakPropertyType? firstSymbolType, WordBreakPropertyType? nexSymbolType)
+            {
+                Add(firstSymbolType);
+                Add(nexSymbolType);
+            }
+        }
+
+        private const int CACHE_SIZE = 3;
         public static IEnumerable<string> GetWords(string text)
         {
+            if (string.IsNullOrEmpty(text))
+                yield break;
+
+            WordBreakPropertyType[] typesCache = new WordBreakPropertyType[CACHE_SIZE];
+            int i = 0;
+            foreach (char c in text)
+            {
+
+            }
             yield return "word";
         }
     }
