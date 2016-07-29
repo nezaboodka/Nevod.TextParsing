bd2a413af4143b0e94d39a177090e8bf799ca069
Ivan <vana.shimko@gmail.com>
2016-07-29 21:59:32 +0300

    Add tests.

--- WordExtractionTests/WordExtractorTests.cs
+++ WordExtractionTests/WordExtractorTests.cs
@@ -6,16 +6,62 @@ namespace WordExtraction.Tests
     [TestClass]
     public class WordExtractorTests
     {
-        [TestMethod]
-        public void GetWords_SiteExample()
+        private string[] ExtractWords(string text)
         {
             var wordExtractor = new WordExtractor();
-            string UNICODE_DEMO = "The quick(\"brown\") fox can't jump 32.3 feet, right.";
+            string[] result = wordExtractor.GetWords(text).ToArray();
+            return result;
+        }
+
+        private void PerformTest(string testString, string[] expectedResult)
+        {
+            string[] result = ExtractWords(testString);
+            CollectionAssert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void SiteExample()
+        {
+            string testString = "The quick(\"brown\") fox can't jump 32.3 feet, right.";
             string[] expectedResult = { "The", "quick", "brown", "fox", "can't", "jump", "32.3", "feet", "right" };
 
-            string[] result = wordExtractor.GetWords(UNICODE_DEMO).ToArray();
+            PerformTest(testString, expectedResult);
+        }
 
-            CollectionAssert.AreEqual(result, expectedResult);
+        [TestMethod]
+        public void OneWord()
+        {
+            string testString = "word";
+            string[] expectedResult = { "word" };
+
+            PerformTest(testString, expectedResult);
+        }
+
+        [TestMethod]
+        public void EmptyString()
+        {
+            string testString = "";
+            string[] expectedResult = {};
+
+            PerformTest(testString, expectedResult);
+        }
+
+        [TestMethod]
+        public void Null()
+        {
+            string testString = null;
+            string[] expectedResult = {};
+
+            PerformTest(testString, expectedResult);
+        }
+
+        [TestMethod]
+        public void Сyrillic()
+        {
+            string testString = "1А класс; 56,31 световых года, потом 100500 45.10 дней!";
+            string[] expectedResult = { "1А", "класс", "56,31", "световых", "года", "потом", "100500", "45.10", "дней" };
+
+            PerformTest(testString, expectedResult);
         }
     }
 }
