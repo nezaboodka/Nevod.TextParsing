5153fe69ff51475fd88bf637c954db6e154898e3
Ivan <vana.shimko@gmail.com>
2016-11-14 17:48:20 +0300

    Update Tokenizer tests for new requirements

--- Source/TokenizerTests/TokenizerTests.cs
+++ Source/TokenizerTests/TokenizerTests.cs
@@ -10,10 +10,33 @@ namespace TextParser.Tests
     public class TokenizerTests
     {
         [TestMethod]
-        public void SiteExample()
+        public void Latin()
         {
-            string testString = "The quick(\"brown\") fox can't jump 32.3 feet, right.";
-            string[] expectedResult = { "The", "quick", "brown", "fox", "can't", "jump", "32.3", "feet", "right" };
+            string testString = "The (\"brown\") can't 32.3 feet, right?";
+            Token[] expectedResult =
+            {
+                new Token("The".Slice(), Token.Alphabetic),
+                new Token(" ".Slice(), Token.WhiteSpace),                 
+                new Token("(".Slice(), Token.Symbol), 
+                new Token("\"".Slice(), Token.Symbol), 
+                new Token("brown".Slice(), Token.Alphabetic),
+                new Token("\"".Slice(), Token.Symbol),
+                new Token(")".Slice(), Token.Symbol),                
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("can".Slice(), Token.Alphabetic),
+                new Token("'".Slice(), Token.Symbol),
+                new Token("t".Slice(), Token.Alphabetic),
+                new Token(" ".Slice(), Token.WhiteSpace),                
+                new Token("32".Slice(), Token.Numeric),
+                new Token(".".Slice(), Token.Symbol),
+                new Token("3".Slice(), Token.Numeric),
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("feet".Slice(), Token.Alphabetic),
+                new Token(",".Slice(), Token.Symbol),
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("right".Slice(), Token.Alphabetic),
+                new Token("?".Slice(), Token.Symbol),
+            };
             PerformEqualityTest(testString, expectedResult);
         }
 
@@ -21,7 +44,10 @@ namespace TextParser.Tests
         public void OneWord()
         {
             string testString = "word";
-            string[] expectedResult = { "word" };
+            Token[] expectedResult =
+            {
+                new Token("word".Slice(), Token.Alphabetic) 
+            };
             PerformEqualityTest(testString, expectedResult);
         }
 
@@ -29,7 +55,7 @@ namespace TextParser.Tests
         public void EmptyString()
         {
             string testString = "";
-            string[] expectedResult = { };
+            Token[] expectedResult = { };
             PerformEqualityTest(testString, expectedResult);
         }
 
@@ -37,15 +63,35 @@ namespace TextParser.Tests
         public void Null()
         {
             string testString = null;
-            string[] expectedResult = { };
+            Token[] expectedResult = { };
             PerformEqualityTest(testString, expectedResult);
         }
 
         [TestMethod]
         public void Cyrillic()
         {
-            string testString = "1А класс; 56,31 светового года, потом 45.1 дня!";
-            string[] expectedResult = { "1А", "класс", "56,31", "светового", "года", "потом", "45.1", "дня" };
+            string testString = "1А класс; 56,31 светового, 45.1 дня!";
+            Token[] expectedResult =
+            {
+                new Token("1А".Slice(), Token.Alphanumeric),
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("класс".Slice(), Token.Alphabetic),
+                new Token(";".Slice(), Token.Symbol),
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("56".Slice(), Token.Numeric),
+                new Token(",".Slice(), Token.Symbol),
+                new Token("31".Slice(), Token.Numeric),
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("светового".Slice(), Token.Alphabetic),
+                new Token(",".Slice(), Token.Symbol),
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("45".Slice(), Token.Numeric),
+                new Token(".".Slice(), Token.Symbol),
+                new Token("1".Slice(), Token.Numeric),
+                new Token(" ".Slice(), Token.WhiteSpace),
+                new Token("дня".Slice(), Token.Alphabetic),
+                new Token("!".Slice(), Token.Symbol),
+            };
             PerformEqualityTest(testString, expectedResult);
         }
 
@@ -53,7 +99,10 @@ namespace TextParser.Tests
         public void OneSymbol()
         {
             string testString = "L";
-            string[] expectedResult = { "L" };
+            Token[] expectedResult =
+            {
+                new Token("L".Slice(), Token.Alphabetic), 
+            };
             PerformEqualityTest(testString, expectedResult);
         }
 
@@ -61,44 +110,48 @@ namespace TextParser.Tests
         public void FormatCharacters()
         {
             string testString = "a\u0308b\u0308cd 3.4";
-            string[] expectedResult = { "a\u0308b\u0308cd", "3.4" };
+            Token[] expectedResult =
+            {
+                new Token("a\u0308b\u0308cd".Slice(), Token.Symbol),
+                new Token(" ".Slice(), Token.WhiteSpace), 
+                new Token("3".Slice(), Token.Numeric),
+                new Token(".".Slice(), Token.Symbol),
+                new Token("4".Slice(), Token.Numeric), 
+            };
             PerformEqualityTest(testString, expectedResult);
         }
 
         [TestMethod]
         public void MultipleEnumeration()
         {
-            //string testString = "word word";
-            //string[] expectedResult = {"word", "word"};
-            //var tokenizer = new Tokenizer();
-            //IEnumerable<TokenizerResult> enumerable = tokenizer.GetTokens(testString);
-            //string[] firstResult = TokenEnumerableToStringArray(enumerable);
-            //string[] secondResult = TokenEnumerableToStringArray(enumerable);
-            //CollectionAssert.AreEqual(firstResult, expectedResult);
-            //CollectionAssert.AreEqual(secondResult, expectedResult);
-            throw new NotImplementedException();
+            string testString = "word word";
+            Token[] expectedResult =
+            {
+                new Token("word".Slice(), Token.Alphabetic),
+                new Token(" ".Slice(), Token.WhiteSpace), 
+                new Token("word".Slice(), Token.Alphabetic)
+            };
+            var tokenizer = new Tokenizer();
+            IEnumerable<Token> enumerable = tokenizer.GetTokens(testString);
+            Token[] firstResult = enumerable.ToArray();
+            Token[] secondResult = enumerable.ToArray();
+            CollectionAssert.AreEqual(firstResult, expectedResult);
+            CollectionAssert.AreEqual(secondResult, expectedResult);
         }
 
         // Static internals
 
-        private static void PerformEqualityTest(string testString, string[] expectedResult)
+        private static void PerformEqualityTest(string testString, Token[] expectedResult)
         {
-            //string[] result = ExtractWords(testString);
-            //CollectionAssert.AreEqual(result, expectedResult);
-            throw new NotImplementedException();
+            Token[] result = Tokenize(testString);
+            CollectionAssert.AreEqual(result, expectedResult);
         }
 
-        private static string[] ExtractWords(string text)
+        private static Token[] Tokenize(string text)
         {
-            //var tokenizer = new Tokenizer();
-            //string[] result = TokenEnumerableToStringArray(tokenizer.GetTokens(text));
-            //return result;
-            throw new NotImplementedException();
-        }
-
-        private static string[] TokenEnumerableToStringArray(IEnumerable<TokenizerResult> sliceEnumerable)
-        {
-            throw new NotImplementedException();
+            var tokenizer = new Tokenizer();
+            Token[] result = tokenizer.GetTokens(text).ToArray();
+            return result;
         }
     }
 }
