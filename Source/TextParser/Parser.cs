ca9dfe19992a118acba517069ad02b54cbacb477
Ivan <vana.shimko@gmail.com>
2016-12-05 22:47:17 +0300

    Move plaintext parsing to separate class

--- Source/TextParser/Parser.cs
+++ Source/TextParser/Parser.cs
@@ -3,78 +3,17 @@ using Sharik.Text;
 
 namespace TextParser
 {
-    public class Parser
+    public abstract class Parser
     {
-        private readonly WordBreaker fWordBreakerState;
-        private readonly TokenClassifier fTokenClassifier;
-        private readonly string fText;
-        private int fCurrentPosition;
-        private int fTokenStart;
-        private readonly ParsedText fParsedText;
-
         public static ParsedText GetTokensFromPlainText(string text)
         {
             if (text == null) throw new ArgumentNullException(nameof(text));
-            var parser = new Parser(text);
-            return parser.GetTokensFromPlainText();
+            Parser parser = new PlainTextParser(text);
+            return parser.Parse();
         }
 
         // Internals
 
-        private Parser(string text)
-        {
-            fText = text;
-            fWordBreakerState = new WordBreaker(text);
-            fTokenClassifier = new TokenClassifier();
-            fParsedText = new ParsedText();
-            fParsedText.AddXhtmlElement(text, true);
-            fCurrentPosition = -1;
-            fTokenStart = 0;
-        }
-        
-        protected virtual ParsedText GetTokensFromPlainText()
-        {
-            while (NextCharacter())
-            {
-                if (IsBreak())
-                {
-                    NextToken();
-                }
-            }                
-
-            return fParsedText;
-        }
-
-        protected virtual bool NextCharacter()
-        {
-            bool result = false;
-            fCurrentPosition++;
-            if (fCurrentPosition < fText.Length)
-            {
-                fWordBreakerState.NextCharacter();
-                fTokenClassifier.AddCharacter(fText[fCurrentPosition]);
-                result = true;
-            }
-            return result;
-        }
-
-        protected virtual bool IsBreak()
-        {
-            return fWordBreakerState.IsBreak();
-        }
-
-        protected virtual void NextToken()
-        {
-            var token = new Token
-            {
-                TokenKind = fTokenClassifier.TokenKind,
-                XhtmlIndex = 0,
-                StringPosition = fTokenStart,
-                StringLength = fCurrentPosition - fTokenStart + 1
-            };
-            fParsedText.AddToken(token);
-            fTokenStart = fCurrentPosition + 1;
-            fTokenClassifier.Reset();
-        }
+        protected abstract ParsedText Parse();                
     }
 }
---
+++ Source/TextParser/PlainTextParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace TextParser
+{
+    internal class PlainTextParser : Parser
+    {
+        private readonly WordBreaker fWordBreakerState;
+        private readonly TokenClassifier fTokenClassifier;
+        private readonly string fText;
+        private int fCurrentPosition;
+        private int fTokenStart;
+        private readonly ParsedText fParsedText;
+
+        internal PlainTextParser(string text)
+        {
+            fText = text;
+            fWordBreakerState = new WordBreaker(text);
+            fTokenClassifier = new TokenClassifier();
+            fParsedText = new ParsedText();
+            fParsedText.AddXhtmlElement(text, true);
+            fCurrentPosition = -1;
+            fTokenStart = 0;
+        }
+
+        protected override ParsedText Parse()
+        {
+            while (NextCharacter())
+            {
+                if (IsBreak())
+                {
+                    NextToken();
+                }
+            }
+
+            return fParsedText;
+        }
+
+        private bool NextCharacter()
+        {
+            bool result = false;
+            fCurrentPosition++;
+            if (fCurrentPosition < fText.Length)
+            {
+                fWordBreakerState.NextCharacter();
+                fTokenClassifier.AddCharacter(fText[fCurrentPosition]);
+                result = true;
+            }
+            return result;
+        }
+
+        private bool IsBreak()
+        {
+            return fWordBreakerState.IsBreak();
+        }
+
+        private void NextToken()
+        {
+            var token = new Token
+            {
+                TokenKind = fTokenClassifier.TokenKind,
+                XhtmlIndex = 0,
+                StringPosition = fTokenStart,
+                StringLength = fCurrentPosition - fTokenStart + 1
+            };
+            fParsedText.AddToken(token);
+            fTokenStart = fCurrentPosition + 1;
+            fTokenClassifier.Reset();
+        }
+    }
+}
