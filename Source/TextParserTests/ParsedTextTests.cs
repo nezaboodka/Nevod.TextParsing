00a324ea76885a9f8f2558d23ddb691543838f86
Ivan <vana.shimko@gmail.com>
2017-01-22 22:13:48 +0300

    [TextParserTests]: Add test for GetPlainText for Tag

--- Source/TextParserTests/ParsedTextTests.cs
+++ Source/TextParserTests/ParsedTextTests.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 using TextParser.Common;
 
 namespace TextParser.Tests
@@ -8,12 +10,12 @@ namespace TextParser.Tests
     public class ParsedTextTests
     {
         private static readonly string[] Xhtml = { "<p>", "Hello, ", "<b>", "my w", "</b>", "orld!", "</p>" };        
-        private static readonly ISet<int> PlainTextInXhtml = new HashSet<int> { 1, 3, 5 };
+        private static readonly ISet<int> PlainTextInXhtml = new HashSet<int> { 1, 3, 5 };        
 
         // Public
 
         [TestMethod]
-        public void TokenInSinglePlainTextElement()
+        public void PlainTextForTokenInSinglePlainTextElement()
         {
             ParsedText parsedText = CreateParsedText(Xhtml, PlainTextInXhtml);
             Token testToken = new Token
@@ -31,7 +33,7 @@ namespace TextParser.Tests
         }
 
         [TestMethod]
-        public void CompoundToken()
+        public void PlainTextForCompoundToken()
         {
             ParsedText parsedText = CreateParsedText(Xhtml, PlainTextInXhtml);
             Token testToken = new Token
@@ -48,7 +50,7 @@ namespace TextParser.Tests
         }
 
         [TestMethod]
-        public void PlainText()
+        public void AllPlainText()
         {
             ParsedText parsedText = CreateParsedText(Xhtml, PlainTextInXhtml);
 
@@ -56,6 +58,48 @@ namespace TextParser.Tests
             string actual = parsedText.GetAllPlainText();
 
             Assert.AreEqual(expected, actual);;
+        }
+
+        [TestMethod]
+        public void PlainTextForTag()
+        {
+            string[] xhtml = { "<html>", "<p>", "First paragraph", "</p>", "<p>", "Second paragraph", "</p>", "</html>" };
+            HashSet<int> plainTextInXhtml = new HashSet<int> { 2, 5 };
+            ParsedText parsedText = CreateParsedText(xhtml, plainTextInXhtml);
+            Tag[] testTags = {
+                new Tag
+                {
+                    TagName = string.Empty,
+                    TokenPosition = 0,
+                    TokenLength = 3
+
+                },
+                new Tag
+                {
+                    TagName = string.Empty,
+                    TokenPosition = 3,
+                    TokenLength = 3
+                }
+            };
+            Token[] tokens =
+            {
+                new Token {XhtmlIndex = 2, StringPosition = 0, StringLength = 5, TokenKind = TokenKind.Alphabetic},
+                new Token {XhtmlIndex = 2, StringPosition = 5, StringLength = 1, TokenKind = TokenKind.WhiteSpace},
+                new Token {XhtmlIndex = 2, StringPosition = 6, StringLength = 9, TokenKind = TokenKind.Alphabetic},
+                new Token {XhtmlIndex = 5, StringPosition = 0, StringLength = 6, TokenKind = TokenKind.Alphabetic},
+                new Token {XhtmlIndex = 5, StringPosition = 6, StringLength = 1, TokenKind = TokenKind.WhiteSpace},
+                new Token {XhtmlIndex = 5, StringPosition = 7, StringLength = 9, TokenKind = TokenKind.Alphabetic}
+            };
+            foreach (Token token in tokens)
+            {
+                parsedText.AddToken(token);
+            }
+
+            string[] expected = { "First paragraph", "Second paragraph" };
+            string[] actual = testTags.Select(tag => parsedText.GetPlainText(tag)).ToArray();
+
+            CollectionAssert.AreEqual(expected, actual);
+
         }
 
         // Internal
